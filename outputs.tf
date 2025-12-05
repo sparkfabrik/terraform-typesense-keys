7a6ac9d8f5f99dfe0275ff653feeebf4fa9e1b62
Stefano Sibilia <stefano.sibilia@sparkfabrik.com>
2025-12-05 16:03:22 +0100

    fix: correct indexing in outputs for Kubernetes secret metadata

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "secret_name" {
   description = "The name of the created Kubernetes secret"
-  value       = kubernetes_secret_v1.typesense_credentials.metadata[0].name
+  value       = kubernetes_secret_v1.typesense_credentials[0].metadata[0].name
 }
 
 output "secret_namespace" {
   description = "The namespace of the created Kubernetes secret"
-  value       = kubernetes_secret_v1.typesense_credentials.metadata[0].namespace
+  value       = kubernetes_secret_v1.typesense_credentials[0].metadata[0].namespace
 }
